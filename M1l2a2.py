74b65a5d57f742156a685469fcad5e5e015e2996
vidyakamath0618-wq <vidyakamath0618@gmail.com>
2026-01-09 17:14:30 +0000

    Add files via upload

---
+++ M1l2a2.py
@@ -0,0 +1,10 @@
+#creating different variables
+x = 5
+y = "john"
+print(x)
+print(y)
+codingal = "codingal"
+print(codingal)
+#user input
+name = input("enter your name:")
+print("\nhello", name, "\nwelcome to codingal")
