91d7497347bd61c58d90fc174bb3c40ffc77a6fb
vidyakamath0618-wq <vidyakamath0618@gmail.com>
2026-01-15 21:52:26 +0000

    Add files via upload

---
+++ M1l3a2.py
@@ -0,0 +1,21 @@
+#assigning different variables
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+#printing different variable and their data type
+print("name:", type(name) )
+print("data type of name is", type(name))
+print("age:", type(age))
+print("data type of age is", type(age))
+print("is_student:", type(is_student))
+print("data type of is_student is", type(is_student))
+print("weight:", type(weight))
+print("data type of weight is", type(weight))
+print("\n After Typer Casting ....")
+age = str(age)
+print(age)
+print("data type of age is", type(age))
+weight= int(weight)
+print(weight)
+print("data type of weight is", type(weight))     
