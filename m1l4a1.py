26ba24cb1feb0efdc049077dd64dd9ce377d70fc
vidyakamath0618-wq <vidyakamath0618@gmail.com>
2026-01-16 16:50:42 +0000

    Add files via upload

---
+++ m1l4a1.py
@@ -0,0 +1,12 @@
+#Storing all values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+#finding total of the trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all trees:", sum)
+#finding the average of trees
+average = sum/5
+print("average of all trees:", average)
