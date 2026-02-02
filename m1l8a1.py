6bc423824f5b0619f052bb043b1edb150286faed
vidyakamath0618-wq <vidyakamath0618@gmail.com>
2026-02-02 16:46:18 +0000

    Add files via upload

---
+++ m1l8a1.py
@@ -0,0 +1,12 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z =("value of (v+w)*x/y is",z)
+name = "alex"
+age = 0
+if name =="Alex" or name=="john"and name >= 2:
+    print("hello!welcome.")
+else:
+    print("good bye!")
