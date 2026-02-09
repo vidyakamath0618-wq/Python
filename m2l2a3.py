198212faf91d5e171b075a00e3b49165c858872b
vidyakamath0618-wq <vidyakamath0618@gmail.com>
2026-02-09 17:04:08 +0000

    Add files via upload

---
+++ m2l2a3.py
@@ -0,0 +1,7 @@
+#input greater than one
+n = int(input("enter the value of n:"))
+#print the numbers from n to 1
+print("numbers from{0} to {1}are:".format(n,1))
+#loop to print numbers
+for i in range(n,0,-1):
+    print(i)
