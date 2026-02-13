9296c66259cf9d8b91a9d4cd8bbe7a1a326f29ba
vidyakamath0618-wq <vidyakamath0618@gmail.com>
2026-02-13 16:50:45 +0000

    Add files via upload

---
+++ m2l3a1.py
@@ -0,0 +1,8 @@
+#input the value of terms
+n = int(input("enter the value of terms:"))
+sum = 0 #intialise
+i = 1 #initialise
+while i <= n: #loop will run from 1 to n
+    sum = sum + i
+    i = i + 1
+print("\nSum=",sum)
