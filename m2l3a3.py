0e2416272df51e8a617778d509f39c9a57204554
vidyakamath0618-wq <vidyakamath0618@gmail.com>
2026-02-13 17:04:00 +0000

    Add files via upload

---
+++ m2l3a3.py
@@ -0,0 +1,15 @@
+#take input from user
+num = int(input("enter a number:"))
+#initialize sum
+sum = 0
+#find the sum of the cube of each digit
+temp = num
+while temp >0:
+    digit = temp %10
+    sum += digit**3 
+    temp //= 10
+#display the result
+if num == sum:
+    print(num,"is an armstrong number")
+else:
+    print(num,"is not an armstrong number")
