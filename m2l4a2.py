3babbabb2942857225afcb74dec75797e80f6341
vidyakamath0618-wq <vidyakamath0618@gmail.com>
2026-02-23 16:57:31 +0000

    Add files via upload

---
+++ m2l4a2.py
@@ -0,0 +1,13 @@
+#take two input from user
+lower=int(input("enter a upper range:"))
+upper=int(input("enter a lower range"))
+print("prime numbers between", lower,"and", upper,"are:")
+#iterate loop from lower limit to upper limit
+for num in range(lower,upper + 1):
+    #all prime numbers are greater than one
+    if num > 1:
+        for i in range (2,num):
+            if(num % i)== 0:
+                break
+            else:
+                print(num)
