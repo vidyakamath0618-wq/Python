776aad3fe7e42167736766b9985e47a6dab9a6e5
vidyakamath0618-wq <vidyakamath0618@gmail.com>
2026-03-16 16:44:44 +0000

    Add files via upload

---
+++ m2l10a1.py
@@ -0,0 +1,11 @@
+#take user input
+a = input ("enter a word:")
+#program to check break keyword
+for i in a: #iterate loop
+    if (i=='A'): #condition 1
+        #display result
+        print("A is found")
+        break #break statement
+    else:
+        #display result
+        print("A is not found")
