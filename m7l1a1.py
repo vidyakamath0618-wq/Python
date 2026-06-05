d1e57e505ef273acd7deac7e0f118c3f27121eb2
vidyakamath0618-wq <vidyakamath0618@gmail.com>
2026-06-05 16:41:59 +0100

    Add files via upload

---
+++ m7l1a1.py
@@ -0,0 +1,9 @@
+#import necessary libraries
+from tkinter import *
+#create window
+window = Tk()
+#set the window title and geometry
+window.title('demo window')
+window.geometry('400x300')
+#start the GUI event loop
+window.mainloop()
